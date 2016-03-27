68f6973ac1bdfb5c20eee53053651b61bebeadf9
Drew <andrewjcrutchfield@gmail.com>
2016-03-27 14:51:23 -0400

    Added billing function

--- me_nfapi.py
+++ me_nfapi.py
@@ -19,12 +19,12 @@ class netflow_api:
 	ENCRYPTED_PWORD_URI = '/servlets/SettingsServlet?requestType=AJAX&EncryptPassword={0:s}&sid=0.28584800255841862'
 	AUTH_PAYLOAD = 'AUTHRULE_NAME=Authenticator&clienttype=html&ScreenWidth=2272&ScreenHeight=1242&loginFromCookieData=false&ntlmv2=false&j_username={0:s}&j_password={1:s}&signInAutomatically=on&uname='
 	GET_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0",
-    "Accept-Encoding": "gzip, deflate, sdch",
+	"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0",
+	 "Accept-Encoding": "gzip, deflate, sdch",
 	"Cache-Control": "max-age=0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Connection": "keep-alive",
+	 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	 "Accept-Language": "en-US,en;q=0.5",
+	 "Connection": "keep-alive",
 	}
 
 	def __init__(self, hostname, api_key, user, password, port='8080', protocol='http'):
