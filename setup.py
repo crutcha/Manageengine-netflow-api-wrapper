57a48ca6d639e510157d5a2c085be6156d1a0f6d
Drew <andrewjcrutchfield@gmail.com>
2016-11-13 09:50:25 -0800

    pypi files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'manageengine-api',
+  packages = ['manageengine-api'],
+  version = '0.1',
+  description = 'Wrapper for interacting with ManageEngine Netflow Analyzer v12',
+  author = 'Andrew Crutchfield',
+  author_email = 'andrewjcrutchfield@gmail.com',
+  url = 'https://github.com/crutcha/Manageengine-netflow-api-wrapper', 
+  download_url = 'https://github.com/crutcha/Manageengine-netflow-api-wrapper/tarball/0.1',
+  classifiers = [],
+)
